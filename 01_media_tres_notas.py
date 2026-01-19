945579779934e9981faa5d460f71764735747ce6
Bianka Brito <bianka.brito2005@gmail.com>
2026-01-19 15:38:55 -0300

    Add script to calculate the average of three grades

---
+++ 01_media_tres_notas.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+soma = n1 + n2+ n3  #melhor colocar em () a soma e a divisao fora (n1 + n2+ n3) / 3
+media = soma / 3
+print(media)
