c31f436aa9a5614df319e33d93257aace1cf37f8
Bianka Brito <bianka.brito2005@gmail.com>
2026-02-20 13:41:47 -0300

    Add 24_maior_numero_lista.py file

--- 24_maior_numero_lista.py
+++ 24_maior_numero_lista.py
@@ -1,9 +1,8 @@
-def max(entrada): 
-    return lista.sort()
 entrada = int(input('Digite quantos numeros tera na sua lista: '))
 lista = []
 for i in range(entrada):
     l = input(f'digite o numero {i+1} da lista: ')
     lista.append(l)
 print(lista)
+lista.sort()
 print(f'O maior numero da sua lista é: {lista[-1]}')
