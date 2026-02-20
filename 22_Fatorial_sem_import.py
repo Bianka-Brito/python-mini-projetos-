d3739adf862d7125418c0c82ca7544a3fc8ce6d9
Bianka Brito <bianka.brito2005@gmail.com>
2026-02-20 12:39:30 -0300

    Update 22_Fatorial_sem_import.py

--- 22_Fatorial_sem_import.py
+++ 22_Fatorial_sem_import.py
@@ -1,5 +1,5 @@
 num=int(input('Digite um numero: '))
 fat = 1
-for i in range(1,num+1): #+1 para o numero do usuario entrar no loop, se não para 1 numero antes
+for i in range(1,num+1): 
     fat*=i
 print(f'O fatorial do número {num} é: {fat}')
