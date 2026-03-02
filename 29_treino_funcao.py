a5fa11894e95ff5042ffbcd68dc12347a6c88b9c
Bianka Brito <bianka.brito2005@gmail.com>
2026-03-02 11:45:34 -0300

    Add functions for doubling, checking even, and finding max

---
+++ 29_treino_funcao.py
@@ -0,0 +1,22 @@
+"""
+def dobro(numero): 
+    return numero*2
+entrada= int(input('digite um numero: '))
+resultado = dobro(entrada)
+print(resultado)
+"""
+
+"""
+def eh_par(numero): 
+    return numero %2 ==0
+entrada= int(input('Digite um numero: '))
+print(eh_par(entrada))
+"""
+
+"""
+def maior(a,b): 
+    return 'A' if  a > b else 'B'
+a=int(input('digite o número "A": '))
+b= int(input('Digite o número "B": '))
+print(f'O maior número é o: {maior(a,b)}')
+"""
