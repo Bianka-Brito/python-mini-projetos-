82a55709129cb6a6be4d1d4e23040218cfc350d6
Bianka Brito <bianka.brito2005@gmail.com>
2026-04-09 15:06:28 -0300

    Update 31_sistema_de_cadastro_e_analise.py

--- 31_sistema_de_cadastro_e_analise.py
+++ 31_sistema_de_cadastro_e_analise.py
@@ -1,11 +1,13 @@
-curso = ('Python para iniciantes', 3, 60)
-print(curso)
+print('Python para iniciantes', 3, 60)
+print('\n---------------------------------')
 participantes= [{'Nome': 'Bianka', 'Idade': 24, 'Horas de estudo': 10, 'Area de interesse': 'Dados'},
                 {'Nome': 'Lua', 'Idade': 20, 'Horas de estudo': 8, 'Area de interesse': 'Java'},
                 {'Nome': 'Liz', 'Idade': 30, 'Horas de estudo': 12, 'Area de interesse': 'TI'}]
 while True:
     print('1.Cadastrar participante \n2.Mostrar participantes \n3.Mostrar análises \n4.Sair')
+    print('---------------------------------')
     menu = input('Escolha sua opção: ')
+    print('\n---------------------------------')
     if menu == '1': 
         nome = input('Insira o nome da participante: ')
         idade = int(input('Insita a idade da participante: '))
@@ -14,10 +16,12 @@ while True:
         novo = {'Nome': nome, 'Idade': idade, 'Horas de estudo': horas_de_estudo, 'Area de interesse': area_de_interesse}
         participantes.append(novo)
         print(participantes)
+        print('\n---------------------------------')
     
     elif menu == '2':
         for p in participantes:
             print(p)
+        print('-'* 90)
     
     elif menu == '3':
         maiores = 0
@@ -35,7 +39,7 @@ while True:
                 estudos.append(i['Nome']) 
                 
         media = soma_horas / t
-        print(f'--- Análise de dados ---\nTotal de participantes: {t} \nMaiores de idade: {maiores} \nMédia horas de estudos: {media} \nParticipantes com bom ritmo de estudos (>= 5h):{estudos}')
+        print(f'-------- Análise de dados -------- \n--------------------------------- \nTotal de participantes: {t} \nMaiores de idade: {maiores} \nMédia horas de estudos: {media} \nParticipantes com bom ritmo de estudos (>= 5h):{estudos} \n\n---------------------------------')
     
     else:
         break
